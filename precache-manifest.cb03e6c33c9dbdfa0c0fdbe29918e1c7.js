ebea809b6b121a5ca9a3c7fd40235cd847d476d1
Srikanth Khandavally <srikanth@Srikanths-MacBook-Pro.local>
2020-08-25 04:58:23 -0500

    Updates

---
+++ precache-manifest.cb03e6c33c9dbdfa0c0fdbe29918e1c7.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f678da1499dc931c9bb821927abdd58",
+    "url": "/about-texas/index.html"
+  },
+  {
+    "revision": "b2ba96811df1cfc96c4d",
+    "url": "/about-texas/static/css/main.dfb1218f.chunk.css"
+  },
+  {
+    "revision": "b79338ff90ba20c39623",
+    "url": "/about-texas/static/js/2.d3a71e5c.chunk.js"
+  },
+  {
+    "revision": "89ea5e0ed13930f7b9c9c2f392e27cfe",
+    "url": "/about-texas/static/js/2.d3a71e5c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b2ba96811df1cfc96c4d",
+    "url": "/about-texas/static/js/main.fe1d0412.chunk.js"
+  },
+  {
+    "revision": "236061d836abdc296e49",
+    "url": "/about-texas/static/js/runtime-main.c7b13496.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/about-texas/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "de441a0429730ac86ef4e5375c95731d",
+    "url": "/about-texas/static/media/tx-flag.de441a04.png"
+  },
+  {
+    "revision": "bdfc33e961f107f6215910932f467ddd",
+    "url": "/about-texas/static/media/tx-map.bdfc33e9.jpg"
+  },
+  {
+    "revision": "5ff25365c5991fac66fb4a116fe891ef",
+    "url": "/about-texas/static/media/tx-seal.5ff25365.png"
+  }
+]);
